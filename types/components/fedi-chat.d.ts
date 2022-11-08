824ed0093ca6556c26c137aa594e5f00682919f0
willmartian <contact@willmartian.com>
2022-11-07 23:36:28 -0500

    v0.0.6

--- types/components/fedi-chat.d.ts
+++ types/components/fedi-chat.d.ts
@@ -5,9 +5,11 @@ export declare class FediChat extends LitElement {
     hideActionBar: boolean;
     private _repliesTask;
     private _actionTask;
+    private timeAgo;
     connectedCallback(): void;
     private parseSrc;
     private getFullUsername;
+    private getTimeAgo;
     render(): import("lit-html").TemplateResult<1>;
     private renderComment;
     private renderActionBar;
--- types/types.d.ts
+++ types/types.d.ts
@@ -6,6 +6,7 @@ export declare type Comment = {
     favourites_count: number;
     content: string;
     account: Account;
+    created_at: string;
 };
 export declare type CommentsResponse = {
     descendants: Comment[];
